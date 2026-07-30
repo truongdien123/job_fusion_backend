8265f6ea9be29a195283df41b57e9f1595514ee0
Trương Châu Diễn <dienrengoku148@gmail.com>
2026-07-30 10:24:15 +0700

    add feature cronjob change status job posting when expiring

--- src/main/java/com/tma/job_fusion_backend/JobFusionBackendApplication.java
+++ src/main/java/com/tma/job_fusion_backend/JobFusionBackendApplication.java
@@ -2,8 +2,10 @@ package com.tma.job_fusion_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class JobFusionBackendApplication {
 
     public static void main(String[] args) {
